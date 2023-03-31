961f54011bdcde4022866f7a02c871803483f948
Trev Burley <scrivvysolutions@gmail.com>
2023-03-31 11:09:40 +0100

    Added key for maptiler - normally wouldn't commit this to public but it's a demo account...

--- resources/js/app.js
+++ resources/js/app.js
@@ -23,46 +23,7 @@ class MapLibreDemo {
     startMaps() {
         this.map = new maplibregl.Map({
             container: 'map',
-            style: {
-                version: 8,
-                sources: {
-                    osm: {
-                        type: 'raster',
-                        tiles: ['https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'],
-                        tileSize: 256,
-                        attribution: '&copy; OpenStreetMap Contributors',
-                        maxzoom: 19
-                    },
-                    terrainSource: {
-                        type: 'raster-dem',
-                        url: 'https://demotiles.maplibre.org/terrain-tiles/tiles.json',
-                        tileSize: 256
-                    },
-                    hillshadeSource: {
-                        type: 'raster-dem',
-                        url: 'https://demotiles.maplibre.org/terrain-tiles/tiles.json',
-                        tileSize: 256
-                    }
-                },
-                layers: [
-                    {
-                        id: 'osm',
-                        type: 'raster',
-                        source: 'osm'
-                    },
-                    {
-                        id: 'hills',
-                        type: 'hillshade',
-                        source: 'hillshadeSource',
-                        layout: {visibility: 'visible'},
-                        paint: {'hillshade-shadow-color': '#473B24'}
-                    }
-                ],
-                terrain: {
-                    source: 'terrainSource',
-                    exaggeration: 1
-                }
-            },
+            style: 'https://api.maptiler.com/maps/streets/style.json?key=xlnKLcf8f5xsjrStjvL0',
             center: this.startPosition,
             zoom: 11
         });
