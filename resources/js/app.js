61752db583f85a4bcb04f3beab07812614b7beb4
Trev Burley <scrivvysolutions@gmail.com>
2023-03-31 10:46:14 +0100

    Refactored to ES6 Class

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,117 +1,124 @@
 import './bootstrap';
 import maplibregl from 'maplibre-gl'; // or "const maplibregl = require('maplibre-gl');"
 
-let startPosition = [-0.957996, 50.851143];
-let markersStore = [];
+class MapLibreDemo {
+    constructor(defaultPosition) {
+        this.startPosition = defaultPosition;
+        this.markersStore = [];
+        this.map = null;
 
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(getPosition);
-    } else {
-        console.log("Geolocation is not supported by this browser.");
+        this.getLocation();
     }
-}
 
-function getPosition(position) {
-    startPosition = [position.coords.longitude, position.coords.latitude];
-}
+    getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                this.startPosition = [position.coords.longitude, position.coords.latitude];
+            }.bind(this));
+        } else {
+            console.log("Geolocation is not supported by this browser.");
+        }
+    }
 
-window.onload = function () {
-    getLocation();
-
-    const map = new maplibregl.Map({
-        container: 'map',
-        style: {
-            version: 8,
-            sources: {
-                osm: {
-                    type: 'raster',
-                    tiles: ['https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'],
-                    tileSize: 256,
-                    attribution: '&copy; OpenStreetMap Contributors',
-                    maxzoom: 19
+    startMaps() {
+        this.map = new maplibregl.Map({
+            container: 'map',
+            style: {
+                version: 8,
+                sources: {
+                    osm: {
+                        type: 'raster',
+                        tiles: ['https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'],
+                        tileSize: 256,
+                        attribution: '&copy; OpenStreetMap Contributors',
+                        maxzoom: 19
+                    },
+                    terrainSource: {
+                        type: 'raster-dem',
+                        url: 'https://demotiles.maplibre.org/terrain-tiles/tiles.json',
+                        tileSize: 256
+                    },
+                    hillshadeSource: {
+                        type: 'raster-dem',
+                        url: 'https://demotiles.maplibre.org/terrain-tiles/tiles.json',
+                        tileSize: 256
+                    }
                 },
-                terrainSource: {
-                    type: 'raster-dem',
-                    url: 'https://demotiles.maplibre.org/terrain-tiles/tiles.json',
-                    tileSize: 256
-                },
-                hillshadeSource: {
-                    type: 'raster-dem',
-                    url: 'https://demotiles.maplibre.org/terrain-tiles/tiles.json',
-                    tileSize: 256
+                layers: [
+                    {
+                        id: 'osm',
+                        type: 'raster',
+                        source: 'osm'
+                    },
+                    {
+                        id: 'hills',
+                        type: 'hillshade',
+                        source: 'hillshadeSource',
+                        layout: {visibility: 'visible'},
+                        paint: {'hillshade-shadow-color': '#473B24'}
+                    }
+                ],
+                terrain: {
+                    source: 'terrainSource',
+                    exaggeration: 1
                 }
             },
-            layers: [
-                {
-                    id: 'osm',
-                    type: 'raster',
-                    source: 'osm'
-                },
-                {
-                    id: 'hills',
-                    type: 'hillshade',
-                    source: 'hillshadeSource',
-                    layout: {visibility: 'visible'},
-                    paint: {'hillshade-shadow-color': '#473B24'}
-                }
-            ],
-            terrain: {
+            center: this.startPosition,
+            zoom: 11
+        });
+
+        this.map.addControl(
+            new maplibregl.NavigationControl({
+                visualizePitch: true,
+                showZoom: true,
+                showCompass: true
+            })
+        );
+
+        this.map.addControl(
+            new maplibregl.TerrainControl({
                 source: 'terrainSource',
                 exaggeration: 1
-            }
-        },
-        center: startPosition,
-        zoom: 11
-    });
+            })
+        );
+    }
 
-    map.addControl(
-        new maplibregl.NavigationControl({
-            visualizePitch: true,
-            showZoom: true,
-            showCompass: true
-        })
-    );
-
-    map.addControl(
-        new maplibregl.TerrainControl({
-            source: 'terrainSource',
-            exaggeration: 1
-        })
-    );
-
-    map.on('load', function () {
+    addMarkers() {
         window.setInterval(function () {
-                axios.get('/api/getMapCoords/')
-                    .then(function (response) {
-                        placeMarkers(response.data);
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                    });
-            }
+                if (this.map !== null) {
+                    axios.get('/api/getMapCoords/')
+                        .then(function (response) {
+                            const markers = response.data;
+                            this.markersStore.forEach(function (m) {
+                                console.log('Removing', m);
+                                m.remove();
+                            });
+
+                            // Reset the array
+                            this.markersStore = [];
+
+                            // Add the new markers
+                            markers.forEach(function (gpsItem) {
+                                let marker = new maplibregl.Marker({
+                                    color: gpsItem.color,
+                                    draggable: false
+                                }).setLngLat(gpsItem.gps)
+                                    .addTo(this.map);
+
+                                this.markersStore.push(marker);
+
+                                console.log('New Markers: ', this.markersStore);
+                            }.bind(this));
+                        }.bind(this))
+                        .catch(function (error) {
+                            console.log(error);
+                        });
+                }
+            }.bind(this)
             , 1000);
-    });
-
-    function placeMarkers(markers) {
-        markersStore.forEach(function (m) {
-            console.log('Removing', m);
-            m.remove();
-        });
-
-        // Reset the array
-        markersStore = [];
-
-        // Add the new markers
-        markers.forEach(function (gpsItem) {
-            let marker = new maplibregl.Marker({
-                color: gpsItem.color,
-                draggable: false
-            }).setLngLat(gpsItem.gps)
-                .addTo(map);
-            markersStore.push(marker);
-
-            console.log('New Markers: ', markersStore);
-        });
     }
 }
+
+let mapLibreDemo = new MapLibreDemo([-0.957996, 50.851143]);
+mapLibreDemo.startMaps();
+mapLibreDemo.addMarkers();
